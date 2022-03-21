cd86ac3219b63fb235f28e1a78ea066900567b89
Adalberto Sampaio <sampaio.afn@gmail.com>
2022-03-20 21:13:44 -0300

    allow cors

--- app.js
+++ app.js
@@ -10,11 +10,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: ['http://localhost:3000'],
-  })
-);
+app.use(cors());
 
 app.use('/auth', require('./routes/auth.routes'));
 
